ab6c982a10e3aadeac3f434c7f494ec124b89306
GUZ3N <feguzensperb@gmail.comm>
2022-04-27 20:49:20 -0400

    1° commit

---
+++ AppTodo/src/app/home/home.page.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { AlertButton, AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private alertCtrl : AlertController) {}
+
+  async showAdd(){
+    const alert = await this.alertCtrl.create({
+      header: 'O que você deseja fazer?',
+      inputs: [
+        {
+          name: 'tarefa1',
+          type: 'text',
+          placeholder: 'Digite oque deseja fazer  '
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () =>{
+            console.log('Cancelado com sucesso.')
+          }
+        },
+        {
+          text: "Adicionar",
+          handler:() =>{
+            console.log("adicionado com sucesso.")
+          }
+        },
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async unread(item){
+    console.log(item);   
+  }
+
+}
